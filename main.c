b52037a709bcb2f71ca12171cb7d43c12df51ba5
IgiWAT <igor.filipiak22@gmail.com>
2026-01-27 10:29:21 +0100

    dodane semafory + usunięto wczytywanie ENTERA w -i

--- main.c
+++ main.c
@@ -1,5 +1,5 @@
-#define _POSIX_C_SOURCE 200809L // Wymagane dla sigaction i struct siginfo_t
-#define _DEFAULT_SOURCE         // Przywraca widoczność usleep
+#define _POSIX_C_SOURCE 200809L 
+#define _DEFAULT_SOURCE         
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -10,18 +10,21 @@
 #include <string.h>
 #include <sys/ipc.h> 
 #include <sys/shm.h> 
-#include <errno.h> // Biblioteka do obsługi błędów (EINTR)
+#include <sys/sem.h> // Biblioteka semaforów
+#include <errno.h> 
 
-#pragma region ZMIENNE GLOBALNE dla SHARED_MEMORY
+#pragma region ZMIENNE GLOBALNE dla KOMUNIKACJI
 
-#define SHARED_SIZE 4096 // Rozmiar pamieci wspoldzielonej
+#define SHARED_SIZE 4096 
+
+// Definicje dla semaforów
+#define SEM_EMPTY 0 // Semafor pustości (P1 czeka na to, by zapisać)
+#define SEM_FULL  1 // Semafor pełności (P2 czeka na to, by odczytać)
 
 // Struktura w pamieci wspolnej
 typedef struct{
-    volatile int status;     // 0=Puste (P1 moze pisac), 1=Pelne (P2 moze czytac)
-    volatile int czy_koniec; // 0=dzialanie, 1=koniec
+    volatile int czy_koniec; // Nadal potrzebne, by P2 wiedział kiedy wyjść
 
-    // PIDy do komunikacji sygnałowej
     pid_t pid_p1;
     pid_t pid_p2;
     pid_t pid_p3;
@@ -32,6 +35,15 @@ typedef struct{
 
 DaneWspolne *dzielona_pamiec = NULL;
 int shm_id = 0;
+int sem_id = 0; // ID zbioru semaforów
+
+// Unia wymagana przez semctl
+union semun {
+    int val;
+    struct semid_ds *buf;
+    unsigned short *array;
+};
+
 #pragma endregion
 
 #pragma region ZMIENNE GLOBALNE
@@ -43,7 +55,6 @@ volatile int czy_dzialac = 1;
 volatile int czy_wstrzymany = 0; 
 volatile int tryb_hex = 1; 
 
-// Zmienne konfiguracyjne
 int tryb_pracy = 0;
 char *sciezka_do_pliku = NULL;
 
@@ -56,7 +67,11 @@ int konfiguracja_trybow(int argc, char *argv[]);
 void to_hex(const char *ascii, size_t len, char *hex);
 
 void obsluga_sygnalow_extended(int sygn, siginfo_t *info, void *context);
-void zarejestruj_sygnaly();
+void zarejestruj_sygnaly(); // Deklaracja
+
+// Funkcje obsługi semaforów
+void sem_lock(int sem_num);   
+void sem_unlock(int sem_num); 
 
 void proces_1();
 void proces_2();
@@ -64,85 +79,70 @@ void proces_3();
 #pragma endregion
 
 int main(int argc, char *argv[]){
-    // Konfiguracja argumentów
-    if (konfiguracja_trybow(argc, argv) != 0){
-        return 1;
-    }
+    if (konfiguracja_trybow(argc, argv) != 0) return 1;
 
-    #pragma region TWORZENIE PAMIECI WSPOLDZIELONEJ
+    // 1. TWORZENIE PAMIECI WSPOLDZIELONEJ
     shm_id = shmget(IPC_PRIVATE, sizeof(DaneWspolne), 0666 | IPC_CREAT);
-    if(shm_id < 0){
-        perror("Blad shmget");
-        return 1;
-    }
+    if(shm_id < 0){ perror("Blad shmget"); return 1; }
 
     dzielona_pamiec = (DaneWspolne*)shmat(shm_id, NULL, 0);
-    if (dzielona_pamiec == (DaneWspolne*)-1){
-        perror("Blad shmat");
-        return 1;
-    }
+    if (dzielona_pamiec == (DaneWspolne*)-1){ perror("Blad shmat"); return 1; }
 
-    // Inicjalizacja pamięci
-    dzielona_pamiec->status = 0; 
     dzielona_pamiec->czy_koniec = 0;
     memset(dzielona_pamiec->dane, 0, SHARED_SIZE);
-    #pragma endregion
 
-    #pragma region TWORZENIE POTOKU
-    if (pipe(potok) == -1){
-        perror("[MAIN] Błąd tworzenia potoku");
-        return 1;
-    }
-    #pragma endregion
+    // 2. TWORZENIE SEMAFORÓW (2 semafory w zestawie)
+    sem_id = semget(IPC_PRIVATE, 2, 0666 | IPC_CREAT);
+    if (sem_id < 0) { perror("Blad semget"); return 1; }
 
-    #pragma region TWORZENIE PROCESOW
+    // Inicjalizacja semaforów
+    union semun arg;
+    arg.val = 1;
+    semctl(sem_id, SEM_EMPTY, SETVAL, arg); // 1 = Wolne miejsce
+    arg.val = 0;
+    semctl(sem_id, SEM_FULL, SETVAL, arg);  // 0 = Brak danych
+
+    // 3. TWORZENIE POTOKU
+    if (pipe(potok) == -1){ perror("[MAIN] Błąd pipe"); return 1; }
+
+    // 4. TWORZENIE PROCESOW
     P1 = utworz_proces(proces_1, "P1");
     P2 = utworz_proces(proces_2, "P2");
     P3 = utworz_proces(proces_3, "P3");
 
     printf("[MAIN] System uruchomiony. PIDy: P1=%d, P2=%d, P3=%d\n", P1, P2, P3);
     
-    // Zapis PIDów do pliku (dla Managera)
     FILE *plik_pid = fopen("pidy_procesow.txt", "w");
     if (plik_pid) {
         fprintf(plik_pid, "%d %d %d", P1, P2, P3);
         fclose(plik_pid);
-    } else {
-        perror("[MAIN] Nie udało się zapisać pliku z PIDami");
     }
 
-    // Zapis PID-ów do pamięci współdzielonej (dla procesów potomnych)
     dzielona_pamiec->pid_p1 = P1;
     dzielona_pamiec->pid_p2 = P2;
     dzielona_pamiec->pid_p3 = P3;
-    #pragma endregion
 
-    #pragma region ZAMYKANIE KONCOWEK w PIPE (dla P0)
     close(potok[0]);
     close(potok[1]);
-    #pragma endregion
 
-    #pragma region OCZEKIWANIE I SPRZATANIE
-    // P0 czeka na zakończenie wszystkich dzieci
+    // Oczekiwanie na koniec
     while (wait(NULL) > 0); 
 
     printf("[MAIN] Wszystkie procesy zakończone. Sprzątanie...\n");
-
     unlink("pidy_procesow.txt"); 
 
+    // Sprzątanie
     shmdt(dzielona_pamiec);         
     shmctl(shm_id, IPC_RMID, NULL); 
+    semctl(sem_id, 0, IPC_RMID); 
     
-    #pragma endregion
-
     return 0;
 }
 
-// Funkcja pomocnicza do rejestracji sygnałów przez sigaction
 void zarejestruj_sygnaly() {
     struct sigaction sa;
     sa.sa_sigaction = obsluga_sygnalow_extended;
-    sa.sa_flags = SA_SIGINFO; // Kluczowe: pozwala pobrać PID nadawcy
+    sa.sa_flags = SA_SIGINFO; 
     sigemptyset(&sa.sa_mask);
 
     sigaction(SIGTERM, &sa, NULL);
@@ -151,57 +151,71 @@ void zarejestruj_sygnaly() {
     sigaction(SIGALRM, &sa, NULL);
 }
 
-// P1: Czytanie -> Konwersja -> Shared Memory
+// Implementacja operacji na semaforach
+void sem_lock(int sem_num) {
+    struct sembuf sb;
+    sb.sem_num = sem_num;
+    sb.sem_op = -1; 
+    sb.sem_flg = 0;
+    
+    while (semop(sem_id, &sb, 1) == -1) {
+        if (errno == EINTR && czy_dzialac) continue;
+        else break; 
+    }
+}
+
+void sem_unlock(int sem_num) {
+    struct sembuf sb;
+    sb.sem_num = sem_num;
+    sb.sem_op = 1;
+    sb.sem_flg = 0;
+    
+    while (semop(sem_id, &sb, 1) == -1) {
+        if (errno == EINTR && czy_dzialac) continue;
+        else break; 
+    }
+}
+
+// P1: Producent 
 void proces_1() {
     zarejestruj_sygnaly(); 
-
-    // Czekamy na inicjalizację PIDów w pamięci
     while(dzielona_pamiec->pid_p3 == 0) usleep(1000); 
 
     FILE *wejscie = NULL;
     ssize_t odczytane_bajty;
-    
     unsigned char bufor[1024]; 
     char hex_bufor[(sizeof(bufor) * 2) + 1]; 
 
-    if(tryb_pracy==1){ // Klawiatura
+    if(tryb_pracy==1){ 
         wejscie = stdin;
         printf(" -> [P1] Tryb Interaktywny. Wpisuj tekst...\n");    
     }
-    else if(tryb_pracy==2){ // Plik
+    else if(tryb_pracy==2){ 
         wejscie = fopen(sciezka_do_pliku, "r");
-        if(wejscie == NULL){
-            perror(" -> [P1] Błąd pliku");
-            exit(1);
-        }
+        if(!wejscie) exit(1);
     }
-    else if(tryb_pracy==3){ // Urandom
+    else if(tryb_pracy==3){ 
         wejscie = fopen("/dev/urandom", "r");
-        if(wejscie == NULL){
-            perror(" -> [P1] Błąd /dev/urandom");
-            exit(1);
-        }
-        printf(" -> [P1] Tryb ciągły /dev/urandom uruchomiony.\n");
+        if(!wejscie) exit(1);
     }
 
-    // Pętla główna
     while (czy_dzialac){
-        // Obsługa EINTR dla read()
         odczytane_bajty = read(fileno(wejscie), bufor, sizeof(bufor));
-        
         if (odczytane_bajty < 0) {
-            if (errno == EINTR) continue; // Sygnał przerwał, próbujemy ponownie
-            else break; // Prawdziwy błąd, koniec
+            if (errno == EINTR) continue; 
+            else break; 
         }
-        if (odczytane_bajty == 0) break; // EOF (Koniec pliku)
+        if (odczytane_bajty == 0) break; 
 
-        // Obsługa pauzy
-        while(czy_wstrzymany && czy_dzialac) { 
-            usleep(100000); 
-        }
+        while(czy_wstrzymany && czy_dzialac) usleep(100000);
         if(!czy_dzialac) break; 
-        
-        // Konwersja
+
+        // Usuwanie ENTER w trybie Interaktywnym
+        if (tryb_pracy == 1 && odczytane_bajty > 0 && bufor[odczytane_bajty-1] == '\n') {
+            odczytane_bajty--;
+        }
+        if (odczytane_bajty == 0) continue; 
+
         if (tryb_hex == 1){
             to_hex((char*)bufor, odczytane_bajty, hex_bufor);
         }
@@ -210,62 +224,64 @@ void proces_1() {
             hex_bufor[odczytane_bajty] = '\0';
         }
 
-        // Synchronizacja i zapis
-        while(dzielona_pamiec->status == 1 && czy_dzialac){
-            usleep(100); 
+        // Czekaj na wolne miejsce (SEM_EMPTY)
+        sem_lock(SEM_EMPTY);
+        
+        if(!czy_dzialac) {
+            sem_unlock(SEM_FULL); 
+            break; 
         }
-        if(!czy_dzialac) break; 
 
-        // Kopiowanie do pamięci współdzielonej
         strncpy(dzielona_pamiec->dane, hex_bufor, SHARED_SIZE-1);
         dzielona_pamiec->dane[SHARED_SIZE-1]='\0';
 
         if(tryb_hex == 1) dzielona_pamiec->wielkosc_danych = strlen(hex_bufor);
         else dzielona_pamiec->wielkosc_danych = odczytane_bajty;
 
-        // Ustawienie flagi dla P2
-        dzielona_pamiec->status = 1;
+        // Sygnalizuj dane (SEM_FULL)
+        sem_unlock(SEM_FULL);
     }
 
-    // Wyjście z programu
-    while(dzielona_pamiec->status == 1 && czy_dzialac) usleep(1000);
-
-    if(czy_dzialac) {
+    if (czy_dzialac) {
+        sem_lock(SEM_EMPTY);
         dzielona_pamiec->czy_koniec = 1;
-        dzielona_pamiec->status = 1;
+        sem_unlock(SEM_FULL);
+    } else {
+        dzielona_pamiec->czy_koniec = 1;
+        sem_unlock(SEM_FULL);
     }
 
     if(tryb_pracy != 1 && wejscie != NULL) fclose(wejscie);
     exit(0);
 }
 
-// P2: Shared Memory -> Pipe
+// P2: Pośrednik
 void proces_2() {
     zarejestruj_sygnaly(); 
-
     close(potok[0]); 
 
     while(czy_dzialac){
         while(czy_wstrzymany && czy_dzialac) usleep(100000);
         if (!czy_dzialac) break;
 
-        // Czekamy na dane od P1 (status=1)
-        while(dzielona_pamiec->status == 0 && czy_dzialac){
-            usleep(100); 
+        // Czekaj na dane (SEM_FULL)
+        sem_lock(SEM_FULL);
+        
+        if(dzielona_pamiec->czy_koniec == 1) {
+             sem_unlock(SEM_EMPTY); 
+             break;
         }
+        if (!czy_dzialac) break;
 
-        if(dzielona_pamiec->czy_koniec == 1) break;
-
-        // Przekazanie do potoku
         int len = dzielona_pamiec->wielkosc_danych;
         if (len > 0){
             ssize_t written_total = 0;
             while (written_total < len) {
                 ssize_t ret = write(potok[1], dzielona_pamiec->dane + written_total, len - written_total);
                 if (ret == -1) {
-                    if (errno == EINTR) continue; // Przerwano, ponawiamy
+                    if (errno == EINTR) continue; 
                     else {
-                        perror(" -> [P2] Błąd zapisu do potoku");
+                        perror(" -> [P2] Błąd zapisu");
                         czy_dzialac = 0; 
                         break;
                     }
@@ -274,18 +290,17 @@ void proces_2() {
             }
         }
 
-        // Zwolnienie pamięci dla P1
-        dzielona_pamiec->status = 0;
+        // Zwolnij bufor (SEM_EMPTY)
+        sem_unlock(SEM_EMPTY);
     }
 
     close(potok[1]);
     exit(0);
 }
 
-// P3: Pipe -> Ekran
+// P3: Konsument
 void proces_3() {
     zarejestruj_sygnaly(); 
-
     close(potok[1]);
     
     char bufor_pipe[SHARED_SIZE];
@@ -293,14 +308,13 @@ void proces_3() {
     int licznik_jednostek = 0; 
 
     while (czy_dzialac){
-        // Obsługa EINTR dla read z potoku
         bajty = read(potok[0], bufor_pipe, sizeof(bufor_pipe)-1);
 
         if (bajty < 0) {
             if (errno == EINTR) continue; 
-            else break; // Błąd potoku
+            else break; 
         }
-        if (bajty == 0) break; // EOF
+        if (bajty == 0) break; 
 
         while (czy_wstrzymany && czy_dzialac) usleep(100000);
         if (!czy_dzialac) break;
@@ -310,10 +324,9 @@ void proces_3() {
         if (tryb_hex == 1){
             for(int i=0; i<bajty; i+=2){ 
                 if (i+1 < bajty){ 
-                    printf("%c%c ", bufor_pipe[i], bufor_pipe[i+1]); // Spacja po każdej parze
+                    printf("%c%c ", bufor_pipe[i], bufor_pipe[i+1]); 
                     licznik_jednostek++;
-
-                    if (licznik_jednostek >= 15){ // Wymóg: 15 jednostek w wierszu
+                    if (licznik_jednostek >= 15){ 
                         printf("\n");
                         licznik_jednostek = 0;
                     }
@@ -327,8 +340,17 @@ void proces_3() {
             }
         }
         else{ 
-            // Wypisywanie RAW
+            // Tryb RAW
             fwrite(bufor_pipe, 1, bajty, stdout);
+            
+            // --- POPRAWKA ---
+            // Ponieważ P1 usunął ENTER, musimy go dodać sztucznie w P3,
+            // ale TYLKO w trybie interaktywnym, żeby kursor spadł do nowej linii.
+            if (tryb_pracy == 1) {
+                printf("\n");
+            }
+            // ----------------
+            
             fflush(stdout); 
             licznik_jednostek = 0;
         }
@@ -377,25 +399,13 @@ void obsluga_sygnalow_extended(int sygn, siginfo_t *info, void *context){
 
 pid_t utworz_proces(void (*funkcja_procesu)(), const char* nazwa){
     pid_t pid = fork();
-    if(pid == 0){
-        funkcja_procesu();
-        exit(0); 
-    }
-    else if(pid < 0){
-        perror(nazwa);
-        exit(1);
-    }
+    if(pid == 0){ funkcja_procesu(); exit(0); }
+    else if(pid < 0){ perror(nazwa); exit(1); }
     return pid;
 }
 
 int konfiguracja_trybow(int argc, char *argv[]){
-    if (argc < 2){
-        fprintf(stderr, "Uzycie: %s <tryb>\n", argv[0]);
-        fprintf(stderr, " -i           : Tryb interaktywny\n");
-        fprintf(stderr, " -f <plik>    : Odczyt z pliku\n");
-        fprintf(stderr, " -r           : Odczyt z /dev/urandom\n");
-        return -1;
-    }
+    if (argc < 2) return -1;
     if (strcmp(argv[1], "-i") == 0) tryb_pracy = 1;
     else if(strcmp(argv[1], "-f") == 0){
         if (argc < 3) return -1;
